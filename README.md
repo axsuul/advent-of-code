f4fbde58a7a7614c57b5ee8d811cacef09790c21
James Hu <hello@james.hu>
2016-12-06 22:24:20 -0800

    Start 2016

--- README.md
+++
@@ -1,3 +0,0 @@
-# Advent of Code Solutions
-
-My Ruby solutions to [Advent of Code](http://adventofcode.com). 
--- helpers.rb
+++
@@ -1,7 +0,0 @@
-def read_input(day)
-  File.open("day#{day}.txt").read
-end
-
-def read_input_lines(*args)
-  read_input(*args).split("\n")
-end
