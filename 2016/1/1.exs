278bb90aba99488a4250f63bd2a9bd7cb83f6e35
James Hu <hello@james.hu>
2016-12-07 00:06:54 -0800

    Modify day 1

--- 2016/1/1.exs
+++
@@ -1,5 +0,0 @@
-IO.puts "Hello World"
-
-{:ok, file} = File.open "input.txt", [:read]
-line = IO.read file, :line
-IO.puts line
--- 2016/1/1.js
+++ 2016/1/1.js
@@ -2,45 +2,45 @@ fs = require('fs')
 
 let move = function(input) {
   let position = [0, 0];
-	let instructions = input.split(", ");
+  let instructions = input.split(", ");
 
-	// coordinate system [x, y]
-	const movements = [[0, 1], [1, 0], [0, -1], [-1, 0]];
-	let direction = 0 	// facing north initially	
+  // coordinate system [x, y]
+  const movements = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+  let direction = 0   // facing north initially 
 
-	// keep track of positions
-	var history = [];
+  // keep track of positions
+  var history = [];
 
-	instructions.forEach(function(step) {
-		let turn = step[0];
-		let blocks = step.substring(1);
+  instructions.forEach(function(step) {
+    let turn = step[0];
+    let blocks = step.substring(1);
 
-		if (turn == "R") {
-			direction += 1;
-		} else if (turn == "L") {
-			direction -= 1;
-		}
+    if (turn == "R") {
+      direction += 1;
+    } else if (turn == "L") {
+      direction -= 1;
+    }
 
-		if (direction < 0) direction = 3;
-		if (direction > 3) direction = 0;
+    if (direction < 0) direction = 3;
+    if (direction > 3) direction = 0;
 
     let movement = movements[direction];
 
-		for (var i = 0; i < blocks; i++) {
-			position[0] += movement[0];
-			position[1] += movement[1];
+    for (var i = 0; i < blocks; i++) {
+      position[0] += movement[0];
+      position[1] += movement[1];
 
-			let coord = `${position[0]},${position[1]}`
+      let coord = `${position[0]},${position[1]}`
 
-			if (history.indexOf(coord) >= 0) {
-				console.log(`Visited before: ${position}`);
-			}
+      if (history.indexOf(coord) >= 0) {
+        console.log(`Visited before: ${position}`);
+      }
 
-			history.push(coord);
-		}
-	});
+      history.push(coord);
+    }
+  });
 
-	return position;
+  return position;
 }
 
 // test cases
