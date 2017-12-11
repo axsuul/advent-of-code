4def5b2b6e585ba605f7917976fe09a1ecdc3ba0
James Hu <hello@james.hu>
2017-12-11 04:34:52 -0800

    Module import

--- 2017/11/lib/advent_of_code.ex
+++ 2017/11/lib/advent_of_code.ex
@@ -35,16 +35,7 @@ defmodule AdventOfCode.PartA do
 end
 
 defmodule AdventOfCode.PartB do
-  def coord_towards(direction, {x, y, z}) do
-    case direction do
-      "n"  -> {x, y + 1, z - 1}
-      "s"  -> {x, y - 1, z + 1}
-      "ne" -> {x + 1, y, z - 1}
-      "sw" -> {x - 1, y, z + 1}
-      "nw" -> {x - 1, y + 1, z}
-      "se" -> {x + 1, y - 1, z}
-    end
-  end
+  import AdventOfCode.PartA
 
   # Use cube coordinates for hex grid
   defp walk(directions, coord \\ {0, 0, 0}, max_dist \\ 0)
@@ -54,15 +45,6 @@ defmodule AdventOfCode.PartB do
     new_max_dist = if dist > max_dist, do: dist, else: max_dist
 
     walk(rest, coord_towards(direction, {x, y, z}), new_max_dist)
-  end
-
-  def calc_distance({x, y, z}) do
-    round((abs(x) + abs(y) + abs(z))/2)
-  end
-
-  def read_input do
-    File.read!("inputs/input.txt")
-    |> String.split(",")
   end
 
   def solve do
