c6aa2b85936264bf1b2d217cb5087d0efde59425
James Hu <hello@james.hu>
2017-12-13 21:57:47 -0800

    Day 14 Part A

--- 2017/14/lib/advent_of_code.ex
+++ 2017/14/lib/advent_of_code.ex
@@ -1,46 +1,7 @@
 use Bitwise
 
-defmodule AdventOfCode.PartA do
-  defp rearrange([], _), do: []
-  defp rearrange(list, pos) when pos == length(list), do: rearrange(list, 0)
-  defp rearrange(list, pos) do
-    {el, new_list} = List.pop_at(list, pos)
-
-    [el] ++ rearrange(new_list, pos)
-  end
-
-  defp reverse(list, pos, length) do
-    new_list = rearrange(list, pos)
-
-    new_list
-    |> Enum.slice(0, length)
-    |> Enum.reverse
-    |> Enum.concat(Enum.slice(new_list, length, length(new_list) - length + 1))
-    |> rearrange(length(new_list) - pos)
-  end
-
-  defp tie_knot(list, pos, skip_size, []), do: list
-  defp tie_knot(list, pos, skip_size, lengths) when pos >= length(list) do
-    tie_knot(list, pos - length(list), skip_size, lengths)
-  end
-  defp tie_knot(list, pos, skip_size, [length | rest]) do
-    reverse(list, pos, length)
-    |> tie_knot(pos + length + skip_size, skip_size + 1, rest)
-  end
-
-  def solve do
-    lengths =
-      "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118"
-      |> String.split(",")
-      |> Enum.map(&String.to_integer/1)
-
-    Enum.to_list(0..255)
-    |> tie_knot(0, 0, lengths)
-    |> IO.inspect
-  end
-end
-
-defmodule AdventOfCode.PartB do
+# From Day 10
+defmodule KnotHash do
   defp rearrange([], _), do: []
   defp rearrange(list, pos) when pos == length(list), do: rearrange(list, 0)
   defp rearrange(list, pos) do
@@ -104,10 +65,9 @@ defmodule AdventOfCode.PartB do
     num_to_hex(num, pos + 1, count + 1, prefix)
   end
 
-  def solve do
+  def calc(string) do
     lengths =
-      # '106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118'
-      ''
+    to_charlist(string)
       |> Enum.concat([17, 31, 73, 47, 23])
 
     Enum.to_list(0..255)
@@ -115,6 +75,75 @@ defmodule AdventOfCode.PartB do
     |> generate_dense_hash()
     |> Enum.map(&num_to_hex/1)
     |> Enum.join("")
-    |> IO.inspect
+  end
+end
+
+defmodule AdventOfCode do
+  defmodule PartA do
+    def read_input do
+      # "flqrgnkx"
+      "oundnydw"
+    end
+
+    def hex_to_binary(str) when is_binary(str) do
+      hex_to_binary(String.split(str, "", trim: true))
+    end
+    def hex_to_binary([]), do: ""
+    def hex_to_binary([char | rest]) do
+      binary =
+        case char do
+          "0" -> "0000"
+          "1" -> "0001"
+          "2" -> "0010"
+          "3" -> "0011"
+          "4" -> "0100"
+          "5" -> "0101"
+          "6" -> "0110"
+          "7" -> "0111"
+          "8" -> "1000"
+          "9" -> "1001"
+          "a" -> "1010"
+          "b" -> "1011"
+          "c" -> "1100"
+          "d" -> "1101"
+          "e" -> "1110"
+          "f" -> "1111"
+        end
+
+      binary <> hex_to_binary(rest)
+    end
+
+    def grid_key(x, y) do
+      Integer.to_string(x) <> Integer.to_string(y)
+    end
+
+    def build_row(grid, x, _, _, used_count) when x > 127, do: {grid, used_count}
+    def build_row(grid, x, y, binary, used_count) do
+      is_free = if String.at(binary, x) == "0", do: true, else: false
+      new_used_count = if is_free, do: used_count, else: used_count + 1
+
+      Map.put(grid, grid_key(x, y), is_free)
+      |> build_row(x + 1, y, binary, new_used_count)
+    end
+
+    def build_grid(key), do: build_grid(%{}, key, 0, 0)
+    def build_grid(_, _, y, used_count) when y > 127, do: used_count
+    def build_grid(grid, key, y, used_count) do
+      binary =
+        key <> "-" <> Integer.to_string(y)
+        |> KnotHash.calc()
+        |> hex_to_binary
+
+      IO.inspect y
+
+      {new_grid, new_used_count} = build_row(grid, 0, y, binary, used_count)
+      build_grid(new_grid, key, y + 1, new_used_count)
+    end
+
+    def solve do
+      read_input()
+      |> build_grid
+      |> IO.inspect
+    end
   end
 end
